caa6e3de8a68a6632414d1aa5248977db4370615
zzstoatzz <thrast36@gmail.com>
2024-08-13 04:48:01 -0500

    spiffy again

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ from prefect.testing.utilities import prefect_test_harness
 
 @pytest.fixture(autouse=True)  # for all tests in the test suite
 def prefect_db():
-    with prefect_test_harness() as harness:
+    with prefect_test_harness():
         """sets up a temp sandbox prefect database/server for running tests against"""
-        yield harness
+        yield
