c2f7a1ff307ac6b76e30eae74c74b6ca481284c9
zzstoatzz <thrast36@gmail.com>
2024-09-21 22:16:25 -0500

    push example

--- flows/with_special_deps/deploy.py
+++ flows/with_special_deps/deploy.py
@@ -1,5 +1,14 @@
+from pathlib import Path
+
 import emoji
 from prefect import flow
+from prefect.docker import DockerImage
+
+docker_image = DockerImage(
+    name="zzstoatzz/prefect-pack",
+    tag="with-special-deps",
+    dockerfile=str(Path(__file__).parent / "Dockerfile"),
+)
 
 
 @flow
@@ -8,10 +17,19 @@ def emoji_flow(message: str = "Hello, World!"):
 
 
 if __name__ == "__main__":
-    """from repo root:
+    """test it out from repo root (using dockerhub as registry):
+    ```bash
     python flows/with_special_deps/deploy.py
+    prefect deployment run "emoji-flow/emoji-deployment"
+    prefect worker start --pool docker-work
+    ```
     """
     flow.from_source(
         source="https://github.com/zzstoatzz/prefect-pack.git",
-        entrypoint="flows/with_special_deps/main.py:emoji_flow",
-    ).deploy(name="emoji-flow", work_pool_name="docker-work")
+        entrypoint="flows/with_special_deps/deploy.py:emoji_flow",
+    ).deploy(
+        name="emoji-deployment",
+        work_pool_name="docker-work",
+        image=docker_image,
+        push=True,
+    )
