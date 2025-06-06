78f9bff9b31c866da10bc2d03c03dc9edf4e58d4
zzstoatzz <thrast36@gmail.com>
2025-06-06 01:04:48 -0500

    ok

--- flows/use_polars/main.py
+++ flows/use_polars/main.py
@@ -7,25 +7,25 @@ from prefect import flow, task
 
 
 @task
-def make_one_dataframe():
+def make_one_dataframe() -> pl.DataFrame:
     df = pl.DataFrame(
         {
             "name": ["Alice", "Bob", "Charlie"],
             "age": [25, 30, 35],
         }
     )
-    print(df)
+    return df
 
 
 @task
-def modify_one_dataframe(df: pl.DataFrame):
+def modify_one_dataframe(df: pl.DataFrame) -> pl.DataFrame:
     df = df.with_columns(pl.col("age") + 1)
-    print(df)
+    return df
 
 
 @task
 def save_one_dataframe(df: pl.DataFrame):
-    df.write_csv("one_dataframe.csv")
+    df.write_csv("one_dataframe.csv", compression="gzip")
 
 
 @flow(log_prints=True)
@@ -33,6 +33,7 @@ def main():
     df = make_one_dataframe()
     df = modify_one_dataframe(df)
     save_one_dataframe(df)
+    print(df)
 
 
 if __name__ == "__main__":
